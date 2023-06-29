2f9563a999fb2eb4a7f0dfd9dfe449a963ad81e0
Thilina-Pathirage <thilinapathirage@gmail.com>
2023-06-29 17:27:16 +0530

    first commit

---
+++ src/test/java/com/example/gamediscoveryserver/GameDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
